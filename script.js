02f5fcdbab7dc321e10f80f4ae62a3b454aa1c2e
Ricoding <57916204+tallerdeldiablo@users.noreply.github.com>
2021-09-13 18:35:12 -0400

    added the loop for the pass generator
    There are four if statements, 
        ## if the prompt is true the respective array will generate a random character

--- script.js
+++ script.js
@@ -5,18 +5,23 @@ var generateBtn = document.querySelector("#generate");
 
 
 function writePassword() {
+  const upperA = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  const lowerA = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const numberA = [1,2,3,4,5,6,7,8,9];
+  const specialA = [40,41,42,43,44,45,46,47,48];
 
-/*   PROmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmMSS */
+  /*   PROmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmMSS */
 var ifupper ;
 var iflower;
-var caracter ;
+var ifcaracter ;
 var ifnumbers;
 var combinations = 0 ;
+var array = ["","","","","","","",""];
 
 
 var longe = prompt("How Long between 8 and 128");
 // If is not a number or between 8-128
-if (longe <9 || longe >128 ||  isNaN(longe))  {
+if (longe <8 || longe >128 ||  isNaN(longe))  {
   alert("ERROR number is not between 8 and 128");
   return
 }
@@ -27,6 +32,13 @@ if (longe <9 || longe >128 ||  isNaN(longe))  {
        if (ifupper == true) {
           ifupper=true;                        //true value upper
             combinations++;
+
+            var index = Math.floor(Math.random() * lowerA.length);
+            array [0] = upperA [index];
+            array [1] = upperA [index];
+
+            console.log("array"+ array);
+
         console.log("yes  upper"+ifupper+combinations);
       }     
       else{
@@ -39,31 +51,40 @@ if (longe <9 || longe >128 ||  isNaN(longe))  {
      if (iflower == true) {
           iflower=true;                       //true value caracter
              combinations++;
+
+          //   array [2] = lowerA [index];
+           // array [3] = lowerA [index];
+
       console.log("Yes caracter"+iflower+combinations);
      }
         else{
           iflower = false;
+                                 
         console.log("NO iflower"+iflower+combinations);
      }
      ifnumbers = confirm("Do you want to use Numbers?");
      if (ifnumbers == true) {
       ifnumbers=true;                       //true value caracter
         combinations++;
+         
+
      console.log("Yes ifnumbers"+ifnumbers+combinations);
     }
        else{
         ifnumbers = false;
+        numberA [0];          //empty array
        console.log("NO ifnumbers"+ifnumbers+combinations);
     }
-    caracter = confirm("Do you want to use Special Caracters?");
-    if (caracter == true) {
-      caracter=true;                       //true value caracter
-        combinations++;
-     console.log("Yes caracter"+caracter+combinations);
+    ifcaracter = confirm("Do you want to use Special Characters?");
+    if (ifcaracter == true) {
+      ifcaracter=true;                          //true value caracter
+        combinations++;  
+
+     console.log("Yes caracter"+ifcaracter+combinations);
     }
        else{
-       caracter = false;
-       console.log("NO caracter"+caracter+combinations);
+       ifcaracter = false;
+            console.log("NO character"+ifcaracter+combinations);
     }
  }
  /*ERROR*/ 
@@ -72,22 +93,49 @@ if (combinations == 0 ){
   return
 }
 
+// Filling the password with every true condition
 
+for (let i = 0; i < longe; i++) {
+  
+  if (ifupper){
+  array[i]= upperA[Math.floor(Math.random() * 25)];
+  i++;
+    }
+     if (iflower==true){
+      array[i]= lowerA[Math.floor(Math.random() * 25)];
+      i++;
+      }
+       if (iflower==true)
+       {
+      array[i]= numberA[Math.floor(Math.random() * 10)];
+      i++;
+        }
+         if (ifcaracter){
+         array[i]= numberA[Math.floor(Math.random() * 10)];
+         i++;
+        }
 
+}
+console.log("PAssword"+array);
 
+generatePassword(array);
 
  /*end prommmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm*/ 
 
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
  function  generatePassword(){
 
 
+return array.join('');
 
 
+
+  
   }
 
 
